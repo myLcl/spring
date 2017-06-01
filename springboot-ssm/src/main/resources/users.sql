0e9109ae1bd40bcb405be559206c7d05405a16ed
myLcl <671247500@qq.com>
2017-06-01 10:23:28 +0800

    update db

--- springboot-ssm/src/main/resources/users.sql
+++ springboot-ssm/src/main/resources/users.sql
@@ -1,29 +1,14 @@
-/*
-Navicat MySQL Data Transfer
+drop database if exists ssm;
+create database ssm;
+use ssm;
 
-Source Server         : 本地
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : test1
-
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
-
-Date: 2016-11-05 21:17:33
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for `users`
--- ----------------------------
 DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE `users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `userName` varchar(32) DEFAULT NULL COMMENT '用户名',
   `passWord` varchar(32) DEFAULT NULL COMMENT '密码',
-  `user_sex` varchar(32) DEFAULT NULL,
+  `user_sex` varchar(32) DEFAULT NULL COMMENT '性别',
   `nick_name` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
