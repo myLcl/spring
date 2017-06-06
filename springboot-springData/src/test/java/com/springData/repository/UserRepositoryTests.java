ed4c1012d54e62513080ca582f0ba1f2ee839724
myLcl <671247500@qq.com>
2017-06-06 15:35:28 +0800

    add logback

--- springboot-springData/src/test/java/com/springData/repository/UserRepositoryTests.java
+++ springboot-springData/src/test/java/com/springData/repository/UserRepositoryTests.java
@@ -3,6 +3,8 @@ package com.springData.repository;
 import com.springData.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -14,11 +16,16 @@ import java.util.List;
 @SpringBootTest
 public class UserRepositoryTests {
 
+	private static final Logger logger = LoggerFactory.getLogger(UserRepositoryTests.class);
 	@Autowired
 	private UserRepository userRepository;
 
 	@Test
 	public void test() throws Exception {
+		logger.debug("debug==============================");
+		logger.error("error==============================");
+		logger.info("info==================================");
+		logger.warn("warn====================================");
 		List<User> userList = userRepository.findAll();
 		for (int i = 0; i < userList.size() ; i++) {
 			System.out.println(userList.get(i));
